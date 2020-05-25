edb9b82c8c24286e912aa19ede69dee5a5c73952
Joe Maddalone <>
2020-05-25 16:26:17 -0500

    firsties

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,45 @@
 import React from 'react'
+import Path from 'react-svg-path'
+import 'react-svg-path-shapes'
 
-import { ExampleComponent } from 'react-svg-path-shapes'
-import 'react-svg-path-shapes/dist/index.css'
+const data = [-10, -45, 10, 0, 35, 10, -9, 25, 15, 75]
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div>
+      <h2>.grid()</h2>
+      <svg width={250} height={250}>
+        <path d={new Path().grid(1, 1, 248, 248, 10, 10).toString()} />
+      </svg>
+      <h2>.square()</h2>
+      <svg width={120} height={120}>
+        <path d={new Path().square(100, 10, 10).toString()} />
+      </svg>
+      <h2>.circle()</h2>
+      <svg width={110} height={120}>
+        <path d={new Path().circle(95, 55, 60).toString()} />
+      </svg>
+      <h2>.polyline()</h2>
+      <svg width={260} height={160}>
+        <path d={new Path().polyline(10, 100, 240, data).toString()} />
+      </svg>
+      <h2>.stepline()</h2>
+      <svg width={800} height={160}>
+        <path d={new Path().stepline(10, 100, 250, data).toString()} />
+      </svg>
+      <h2>combining shapes</h2>
+      <svg width={305} height={155}>
+        <path
+          d={new Path().grid(0, 0, 300, 150, 10, 10).toString()}
+          style={{ stroke: '#ccc' }}
+        />
+        <path
+          d={new Path().polyline(0, 100, 300, data).toString()}
+          style={{ stroke: 'green', strokeWidth: 2 }}
+        />
+      </svg>
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,73 @@
-import React from 'react'
-import styles from './styles.module.css'
+import Path from 'react-svg-path'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+Path.macro('square', function (size, x, y) {
+  if (x && y) {
+    this.M(x, y) // only move if x & y are defined
+  }
+  this.right(size).down(size).left(size).up(size)
+  return this
+})
+
+Path.macro('circle', function (size, x, y) {
+  if (x && y) {
+    this.M(x, y) // only move if x & y are defined
+  }
+  const radius = size / 2
+  this.m(-radius, 0)
+    .a(radius, radius, 0, 1, 0, radius * 2, 0)
+    .a(radius, radius, 0, 1, 0, -(radius * 2), 0)
+  return this
+})
+
+Path.macro('triangle', function (w, h, x, y) {
+  this.M(x, y) // only move if x & y are defined
+    .l(-w / 2, h)
+    .right(w)
+    .L(x, y)
+  return this
+})
+
+Path.macro('grid', function (x, y, width, height, cols, rows, close = true) {
+  const extra = close ? 1 : 0
+  const colWidth = width / cols
+  const rowHeight = height / rows
+  this.M(x, y)
+  if (cols) {
+    Array.from({ length: cols + extra }).map((_, index) =>
+      this.M(x + index * colWidth, y).down(height)
+    )
+  }
+  if (rows) {
+    Array.from({ length: rows + extra }).map((_, index) =>
+      this.M(x, y + index * rowHeight, y).right(width)
+    )
+  }
+  return this
+})
+// [-10, -45, 10, 0, 35, 10, -9, 25, 15, 75]
+Path.macro('polyline', function (x, y, width, data) {
+  this.moveTo(x, y)
+  const pointDistance = parseInt(width / data.length)
+  data.forEach((val, index, arr) => {
+    if (index !== 0) {
+      const prev = arr[index - 1]
+      this.l(pointDistance, prev - val)
+    } else {
+      this.l(pointDistance, -val)
+    }
+  })
+  return this
+})
+
+Path.macro('stepline', function (x, y, width, data) {
+  this.moveTo(x, y)
+  const pointDistance = parseInt(width / data.length)
+  data.forEach((val, index, arr) => {
+    const prev = index === 0 ? 0 : arr[index - 1]
+    this[prev > val ? 'down' : 'up'](Math.abs(prev - val))
+    if (index !== arr.length - 1) {
+      this.right(pointDistance)
+    }
+  })
+  return this
+})
