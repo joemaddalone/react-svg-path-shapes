ff6a9771dd3ee46c74f473fa0bc5c9b13b735b1b
Joe Maddalone <>
2020-05-25 16:27:53 -0500

    Updates

---
+++ precache-manifest.5ec8094092341f96ce58b7077c7b2a4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "787b6e3fbe042415ac7af45f7a8c757c",
+    "url": "/react-svg-path-shapes/index.html"
+  },
+  {
+    "revision": "dbbc0fba05f8112423fc",
+    "url": "/react-svg-path-shapes/static/css/main.16ded3dd.chunk.css"
+  },
+  {
+    "revision": "0c0d0513b90c7137528e",
+    "url": "/react-svg-path-shapes/static/js/2.9c78a927.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-svg-path-shapes/static/js/2.9c78a927.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbbc0fba05f8112423fc",
+    "url": "/react-svg-path-shapes/static/js/main.aea02370.chunk.js"
+  },
+  {
+    "revision": "a1a5439836b73d82f63a",
+    "url": "/react-svg-path-shapes/static/js/runtime-main.3d307e97.js"
+  }
+]);
